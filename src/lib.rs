5be13d60edf21e265bfadb1addba5e4c15b03449
Jasper Bekkers <bekkers@gmail.com>
2022-03-16 19:21:43 +0100

    Move permissions block

--- src/lib.rs
+++ src/lib.rs
@@ -267,15 +267,15 @@ pub fn download_and_extract_packages(
                         } else {
                             let mut outfile = std::fs::File::create(&outpath).unwrap();
                             std::io::copy(&mut file, &mut outfile).unwrap();
+            
+                            #[cfg(unix)]
+                            {
+                                use std::os::unix::fs::PermissionsExt;
+                                if let Some(mode) = file.unix_mode() {
+                                    std::fs::set_permissions(&outpath, std::fs::Permissions::from_mode(mode)).unwrap();
+                                }
+                            }
                         }
-                    }
-                }
-
-                #[cfg(unix)]
-                {
-                    use std::os::unix::fs::PermissionsExt;
-                    if let Some(mode) = file.unix_mode() {
-                        std::fs::set_permissions(&outpath, std::fs::Permissions::from_mode(mode)).unwrap();
                     }
                 }
             }
